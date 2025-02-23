edea439f88051172e5fdeac654d527d1fa33d238
りんご飴196 <kazumac41@gmail.com>
2025-02-23 15:09:34 +0900

    ♻️  :リファクタリング

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/Command.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/commands/Command.kt
@@ -5,11 +5,11 @@ import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
 
-class Command:CommandExecutor,TabCompleter {
+class Command : CommandExecutor, TabCompleter {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         return true
     }
-    
+
     override fun onTabComplete(commandSender: CommandSender, command: Command, label: String, args: Array<out String>): MutableList<String>? {
         return null
     }
